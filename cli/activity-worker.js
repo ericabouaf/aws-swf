4f6aa8dfc0bd7edcc38f73ffd792abca36af7523
Eric Abouaf <eric.abouaf@gmail.com>
2012-10-23 23:06:35 +0200

    indentation

--- cli/activity-worker.js
+++ cli/activity-worker.js
@@ -4,7 +4,7 @@
  * It will look in the working directory for a Node.JS module that has the same name as the activity-type
  */
 var path = require('path'),
-  swf = require('../index');
+    swf = require('../index');
 
 // The task is given to this process as a command line argument in JSON format :
 var taskConfig = JSON.parse(process.argv[2]);
@@ -13,8 +13,8 @@ var accessKeyId = process.argv[3];
 var secretAccessKey = process.argv[4];
 
 var swfClient = swf.createClient({
-  accessKeyId: accessKeyId,
-  secretAccessKey: secretAccessKey
+    accessKeyId: accessKeyId,
+    secretAccessKey: secretAccessKey
 });
 
 var fetchconfigfile = process.argv[5];
@@ -23,10 +23,10 @@ var fetchconfigfile = process.argv[5];
 var task = new swf.ActivityTask(swfClient, taskConfig);
 
 function activityFailed(reason, details) {
-  task.respondFailed(reason, details, function (err) {
-    if (err) { console.error(err); return; }
-    console.log("respond failed !");
-  });
+    task.respondFailed(reason, details, function (err) {
+        if (err) { console.error(err); return; }
+        console.log("respond failed !");
+    });
 }
 
 var workerName = taskConfig.activityType.name;
@@ -34,27 +34,28 @@ var workerName = taskConfig.activityType.name;
 var fetch_config = require(fetchconfigfile).fetch_config;
 
 try {
-  console.log("Trying to load worker : " + workerName);
-  var worker = require(path.join(process.cwd(), workerName)).worker;
-  console.log("module loaded !");
+    console.log("Trying to load worker : " + workerName);
+    var worker = require(path.join(process.cwd(), workerName)).worker;
+    console.log("module loaded !");
 
-  // Use the asynchronous method to get the config for this module
-  fetch_config(workerName, function (err, config) {
+    // Use the asynchronous method to get the config for this module
+    fetch_config(workerName, function (err, config) {
 
-    if (err) {
-      console.log(err);
-      activityFailed("Unable to get config for: " + workerName, "");
-      return;
-    }
+        if (err) {
+            console.log(err);
+            activityFailed("Unable to get config for: " + workerName, "");
+            return;
+        }
 
-    try {
-      worker(task, config);
-    } catch (ex) {
-      console.log(ex);
-      activityFailed("Error executing " + workerName, "");
-    }
-  });
+        try {
+            worker(task, config);
+        } catch (ex) {
+            console.log(ex);
+            activityFailed("Error executing " + workerName, "");
+        }
+    });
+
 } catch (ex) {
-  console.log(ex);
-  activityFailed("Unable to load module " + workerName, "");
+    console.log(ex);
+    activityFailed("Unable to load module " + workerName, "");
 }
--- examples/workflows/parallel-test/parallel-test.js
+++ examples/workflows/parallel-test/parallel-test.js
@@ -1,38 +1,38 @@
+
 // sum, sleep -> echo -> terminate
 
-if( just_started ) {
+if (just_started) {
 
-      // schedule step1(sum) and step2(sleep) in parallel
-      
-      schedule('step1', { activityType: 'sleep' });
-      
-      schedule('step2', {
-         activityType: 'sum',
-         input: {
+    // schedule step1(sum) and step2(sleep) in parallel
+
+    schedule('step1', { activityType: 'sleep' });
+
+    schedule('step2', {
+        activityType: 'sum',
+        input: {
             a: 4, 
             b: 6
-         }
-      });
-      
-   }
-   else if( !completed('step1') || !completed('step2') ) {
+        }
+    });
 
-      waiting_for('step1','step2');
-      
-   }
-   
-   // When both step have completed, schedule step3
-   
-   else if( completed('step1', 'step2') && !scheduled('step3') ) {
+} else if( !completed('step1') || !completed('step2') ) {
 
-      schedule('step3', {
-         activityType: 'echo',
-         input: 'this will be echoed...'
-      });
-      
-   }
-   else if( completed('step3')  ) {
+    waiting_for('step1','step2');
 
-      stop("All done !");
-      
-   }
+}
+
+// When both step have completed, schedule step3
+
+else if( completed('step1', 'step2') && !scheduled('step3') ) {
+
+    schedule('step3', {
+        activityType: 'echo',
+        input: 'this will be echoed...'
+    });
+
+}
+else if( completed('step3')  ) {
+
+    stop("All done !");
+
+}
