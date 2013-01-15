c5c5bc21ce92d62fd71f7887c4da8daebc8f21c1
Eric Abouaf <eric.abouaf@gmail.com>
2013-01-15 23:18:38 +0100

    Human Task now working !

--- activities/humantask/server.js
+++ activities/humantask/server.js
@@ -16,14 +16,15 @@ var app = express();
 // Load configurations
 app.config = require('./config.js');
 
-app.use(express.static(__dirname + '/public'));
+app.configure(function(){
+   app.use(express.static(__dirname + '/public'));
+   app.set('views', __dirname + '/app/views');
+   app.set('view engine', 'ejs');
+   app.set('layout', 'layout'); // defaults to 'layout'     
+   app.use(expressLayouts);
+   app.use(express.bodyParser());
+});
 
-app.set('views', __dirname + '/app/views');
-app.set('view engine', 'ejs');
-
-app.set('layout', 'layout'); // defaults to 'layout'     
-
-app.use(expressLayouts);
 
 // DynamoDB connection
 var client = dynamo.createClient(app.config.awsCredentials), 
@@ -33,55 +34,62 @@ app.db = db;
 
 
 function taskFromToken(req, res, next) {
-  // TODO: make task retrieve as middleware
-  app.db.get("Task")
-    .scan({taskToken: req.param('taskToken') })
-    .get("taskToken", "activityId", "startedEventId", "input", "workflowExecution", "activityType")
-    .fetch(function(err, tasks){
-     
-     if(err) {
-        console.log(err);
-        // TODO
-        return;
-     }
-
-     req.task = tasks[0];
-     next();
-
-  });
+   app.db.get("Task")
+      .scan({taskToken: req.param('taskToken') })
+      .get("taskToken", "activityId", "startedEventId", "input", "workflowExecution", "activityType")
+      .fetch(function(err, tasks){
+      
+      if(err) {
+         res.render('error', { 
+            locals: { 
+               error: err
+            } 
+         });
+         return;
+      }
+      req.task = tasks[0];
+      next();
+   });
 }
 
 /**
-* Task finished
-*/
+ * Task finished
+ */
 app.get('/finished', function(req, res) {
-  res.render('finished', { 
-     locals: { 
-        taskToken: "",
-        action: "humantasks"
-     }
-  });
+   res.render('finished', { 
+      locals: { 
+         taskToken: ""
+      }
+   });
 });
 
 
 /**
-* Display an activity
-*/
+ * Display an activity
+ */
 app.get('/activity/:taskToken', taskFromToken, function(req, res) {
   
-  // TODO: check if task has not timed-out (should be "STARTED")
+   if(!req.task) {
+      res.render('unavailable', { 
+         locals: { 
+            taskToken: ""
+         }
+      });
+      return;
+   }
+
+   // TODO: check if task has not timed-out (should be "STARTED")
   
-  var activityTask = req.task;
-  var input = JSON.parse(activityTask.input);
+   var activityTask = req.task;
+   var input = JSON.parse(activityTask.input);
      
-  if(input.template) {
+   if(input.template) {
       
       var stream = mu.renderText(input.template, input, {});
 
       var str = "";
       stream.on('data', function(data) { str += data; });
       stream.on('end', function() {
-            
          fs.readFile(__dirname+'/app/views/layout.ejs', 'utf-8', function(error, content) {
             res.set('Content-Type', 'text/html');
             var body = ejs.render(content, {
@@ -90,88 +98,79 @@ app.get('/activity/:taskToken', taskFromToken, function(req, res) {
             });
             res.send(body);
          });
-            
       });
         
-  }
-  else {
-     // Render the default task view
-     res.render('defaultTaskView', {
-        locals: { 
-           taskToken: querystring.escape(activityTask.taskToken), 
-           activityTask: activityTask, 
-           action: "humantasks" 
-        } 
-     });
-  }   
+   }
+   else {
+      // Render the default task view
+      res.render('defaultTaskView', {
+         locals: { 
+            taskToken: querystring.escape(activityTask.taskToken), 
+            activityTask: activityTask
+         } 
+      });
+   }   
   
 });
 
 
 /**
-* Webservices to complete or fail an activity task
-*/
+ * Webservices to complete or fail an activity task
+ */
 app.post('/:taskToken/completed', taskFromToken, function(req, res){
   
-  var activityTask = req.task;
+   var activityTask = req.task;
+  
+   AWS.config.update({
+      accessKeyId: app.config.awsCredentials.accessKeyId,
+      secretAccessKey: app.config.awsCredentials.secretAccessKey,
+      region: app.config.region
+   });
      
-      AWS.config.update({
-         accessKeyId: app.config.awsCredentials.accessKeyId,
-         secretAccessKey: app.config.awsCredentials.secretAccessKey,
-         region: app.config.region
+   // send a RespondActivityTaskCompleted
+   var svc = new AWS.SimpleWorkflow();
+   svc.client.respondActivityTaskCompleted({
+      "taskToken": req.param('taskToken'),
+      "result": JSON.stringify( req.body )
+   }, function (awsErr, result) {
+
+      if(awsErr) {
+         console.log("respondActivityTaskCompleted failed : ", awsErr);
+         // TODO: delete task
+         var taskItem = app.db.get("Task").get( {taskToken: req.param('taskToken') } ).destroy(function(err) {
+           
+            res.render('error', { 
+               locals: { 
+                  error: awsErr,
+                  activityTask: null,
+                  taskToken: "no"
+               } 
+            });
+         });
+        
+         return;
+      }
+        
+      // Delete the activity
+      var taskItem = app.db.get("Task").get( {taskToken: req.param('taskToken') } ).destroy(function(err) {
+         if (err) { console.error(err); }
+         res.redirect('/finished');
       });
         
-     // send a RespondActivityTaskCompleted
-
-     var svc = new AWS.SimpleWorkflow();
-
-     svc.client.respondActivityTaskCompleted({
-        "taskToken": req.param('taskToken'),
-        "result": JSON.stringify( req.params )
-     }, function (awsErr, result) {
-
-        if(awsErr) {
-          console.log("respondActivityTaskCompleted failed : ", awsErr);
-           // TODO: delete task
-           var taskItem = app.db.get("Task").get( {taskToken: req.param('taskToken') } ).destroy(function(err) {
-              
-              res.render('error', { 
-                locals: { 
-                   error: awsErr,
-                   activityTask: null,
-                   taskToken: "no",
-                   action: "humantasks" 
-                 } 
-              });
-
-           });
-
-           
-           return;
-        }
-           
-        // Delete the activity
-        var taskItem = app.db.get("Task").get( {taskToken: req.param('taskToken') } ).destroy(function(err) {
-          if (err) { console.error(err); }
-          res.redirect('/finished');
-        });
-           
-     });
-  
+   });
 });
 
 app.post('/:taskToken/canceled', taskFromToken, function(req, res){
-  // TODO: send a RespondActivityTaskCanceled
-  // TODO: delete the activity
-  res.send('Hello World');
+   // TODO: send a RespondActivityTaskCanceled
+   // TODO: delete the activity
+   res.send('Hello World');
 });
 
 app.post('/:taskToken/failed', taskFromToken, function(req, res){
-  // TODO: send a RespondActivityTaskFailed
-  // TODO: delete the activity
-  res.send('Hello World');
+   // TODO: send a RespondActivityTaskFailed
+   // TODO: delete the activity
+   res.send('Hello World');
 });
-
 
 
 // start the server
--- examples/human-test/human-test.js
+++ examples/human-test/human-test.js
@@ -1,27 +1,51 @@
 
-var template = "<h1>Default humantask template</h1>"+
-                  "<ul>"+
-                    "{{#data}}<li>{{label}}</li>{{/data}}"+
-                  "</ul>"+
-                  "<input type='text' name='samplefield' value='Complete !' />"+
-                  "<input type='submit' class='btn btn-primary' value='Complete !' />";
-
 schedule({
     name: 'step1',
     activity: 'humantask',
+    //activity: 'mturk_createHit',
+
     input: {
+
+          /*title : "Vote on Text Improvement",
+          description : "Decide which two small paragraphs is closer to a goal.",
+          reward : 0.01,
+          duration: 3600, // 1 hour
+          maxAssignments : 1,
+          questionXML : '<ExternalQuestion xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2006-07-14/ExternalQuestion.xsd">'+
+                        '<ExternalURL>https://s3.amazonaws.com/test-aws-swf/temp.html</ExternalURL>'+
+                        '<FrameHeight>500</FrameHeight>'+
+                        '</ExternalQuestion>',*/
+
         "email-notification": {
             to: "eric.abouaf@gmail.com",
             subject: "Ceci est un test !"
         },
-        "this": "is",
-        "data": [{label: "un"},{label: "deux"}, {label: "trois"}, {label: "quatre"}],
 
-        template: template
+        "data": [{label: "this"},{label: "list"}, {label: "is"}, {label: "templated"}],
+
+        template: "<h1>Default humantask template</h1>"+
+                "<ul>"+
+                  "{{#data}}<li>{{label}}</li>{{/data}}"+
+                "</ul>"+
+                "<img src='http://groups.csail.mit.edu/uid/turkit/www/nut_people.jpg' alt='description not available'></img>"+
+                "<fieldset>"+
+                  "<legend>Legend</legend>"+
+                  "<label>Label name</label>"+
+                  "<input type='text' placeholder='Type something...' name='firstfield'>"+
+                  "<span class='help-block'>Example block-level help text here.</span>"+
+                  "<label class='checkbox'>"+
+                  "<input type='checkbox' name='mybool'> Check me out"+
+                  "</label>"+
+                  "<button type='submit' class='btn btn-primary'>Submit</button>"+
+                  "</fieldset>"
     }
-});
-
-
+}, {
+      // No timeout
+      heartbeatTimeout: "NONE",
+      scheduleToCloseTimeout: "NONE",
+      scheduleToStartTimeout: "NONE",
+      startToCloseTimeout: "NONE"
+   });
 
 stop({
     after: 'step1',
