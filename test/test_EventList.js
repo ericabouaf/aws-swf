079ccc2b37785f13e9df7f368b39db38e23db9a9
neyric <eric.abouaf@gmail.com>
2014-02-26 12:01:46 +0100

    Putting eventList fixtures into files

--- test/test_EventList.js
+++ test/test_EventList.js
@@ -1,5 +1,7 @@
 
-var assert = require("assert");
+var assert = require('assert'),
+    path = require('path'),
+    fs = require('fs');
 
 var swf = require('../index');
 var EventList = swf.EventList;
@@ -8,44 +10,9 @@ var EventList = swf.EventList;
 describe('EventList', function(){
   describe('#indexOf()', function(){
 
-    var evl = new EventList([
-            {
-              "eventId": 1,
-              "eventTimestamp": 1326592619.474,
-              "eventType": "WorkflowExecutionStarted",
-              "workflowExecutionStartedEventAttributes": {
-                "childPolicy": "TERMINATE",
-                "executionStartToCloseTimeout": "3600",
-                "input": "arbitrary-string-that-is-meaningful-to-the-workflow",
-                "parentInitiatedEventId": 0,
-                "tagList": ["music purchase", "digital", "ricoh-the-dog"],
-                "taskList": {"name": "specialTaskList"},
-                "taskStartToCloseTimeout": "600",
-                "workflowType": {
-                  "name": "customerOrderWorkflow",
-                  "version": "1.0"
-                }
-              }
-            },
-            {
-              "decisionTaskScheduledEventAttributes": {
-                "startToCloseTimeout": "600",
-                "taskList": {"name": "specialTaskList"}
-              },
-              "eventId": 2,
-              "eventTimestamp": 1326592619.474,
-              "eventType": "DecisionTaskScheduled"
-            },
-            {
-            "decisionTaskStartedEventAttributes": {
-              "identity": "Decider01",
-                "scheduledEventId": 2
-              },
-              "eventId": 3,
-              "eventTimestamp": 1326593394.566,
-              "eventType": "DecisionTaskStarted"
-            }
-          ]);
+    var fixtureFile = 'eventlist_noactivity.json';
+    var fixtureData = JSON.parse(fs.readFileSync( path.join(__dirname , '..', 'fixtures', fixtureFile), 'utf8') );
+    var evl = new EventList(fixtureData);
 
     it('is_activity_started', function(){
       assert.equal (false, evl.is_activity_started('test1') );
