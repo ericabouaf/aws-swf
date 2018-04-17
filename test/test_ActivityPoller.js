516496a820817427b6a300a596f0b16ac0524884
Fernando Cheong <fcheong3b@gmail.com>
2018-04-17 12:47:53 -0400

    changes in response

--- test/test_ActivityPoller.js
+++ test/test_ActivityPoller.js
@@ -9,15 +9,17 @@ var pollForActivityTaskCallCount = 0;
 var swfClientMock = {
     pollForActivityTask: function(p, cb) {
         pollForActivityTaskCallCount += 1;
-        
+
         setTimeout(function() {
             cb(null, {
                 taskToken: (pollForActivityTaskCallCount == 2) ? '12345' : undefined
             });
-        }, 10);
+        }, 2000);
     }
 };
 var nullLogger = { info: function() {} };
+var domain = 'dev-mediastream-vms'
+var taskList = 'dev-us-LiveEditor-v1.0'
 
 describe('ActivityPoller', function(){
 
@@ -39,27 +41,29 @@ describe('ActivityPoller', function(){
     it('should start, emit ActivityTask, and stop', function(done) {
 
       var activityPoller = new ActivityPoller({
-        domain: 'test-domain',
+        domain: domain,
         taskList: {
-          name: 'test-taskList'
+          name: taskList
         },
         logger: nullLogger
       }, swfClientMock);
-
+      console.log('here')
+      activityPoller.start();
+      console.log('her2e')
 
       activityPoller.on('activityTask', function(activityTask) {
         activityPoller.stop();
         done();
       });
 
-      activityPoller.start();
+
     });
 
     it('should insntaiate without swfClient', function() {
       var activityPoller = new ActivityPoller({
-        domain: 'test-domain',
+        domain: domain,
         taskList: {
-          name: 'test-taskList'
+          name: taskList
         }
       });
     });
